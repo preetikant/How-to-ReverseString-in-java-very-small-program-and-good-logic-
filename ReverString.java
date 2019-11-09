85d26622963631e09486192ffe4b8ce48dc3cf1b
Preeti Kant <44994724+preetikant@users.noreply.github.com>
2019-11-09 22:29:58 +0530

    Add files via upload

---
+++ ReverString.java
@@ -0,0 +1,10 @@
+class ReverString{
+	public static void main(String[] args) {
+		char str[] = {'p','r','e','e','t','i'};
+	
+	for( int i=str.length-1;i>=0 ;i-- ) {
+		System.out.print(str[i]);
+	}
+	}
+	
+}
